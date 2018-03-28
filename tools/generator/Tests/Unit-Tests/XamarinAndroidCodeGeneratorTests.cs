fce359a31ed049f8881433141a3037c07cff2130
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-03-27 21:59:03 -0500

    [tests] initial tests for Field on CodeGenerator

--- tools/generator/Tests/Unit-Tests/XamarinAndroidCodeGeneratorTests.cs
+++ tools/generator/Tests/Unit-Tests/XamarinAndroidCodeGeneratorTests.cs
@@ -16,6 +16,8 @@ namespace generatortests
 			public TestClass (string baseType, string javaName) : base (new TestBaseSupport (javaName))
 			{
 				this.baseType = baseType;
+
+				SymbolTable.AddType (Name, new SimpleSymbol (null, Name, Name, Name, Name));
 			}
 
 			public override bool IsAbstract => false;
@@ -63,6 +65,41 @@ namespace generatortests
 			public override GenericParameterDefinitionList TypeParameters => typeParameters;
 		}
 
+		class TestField : Field
+		{
+			TestClass type;
+
+			public TestField (TestClass type, string name)
+			{
+				this.type = type;
+				Name = name;
+			}
+
+			public override bool IsDeprecated => false;
+
+			public override string DeprecatedComment => string.Empty;
+
+			public override bool IsFinal => false;
+
+			public override bool IsStatic => false;
+
+			public override string JavaName => Name;
+
+			public override bool IsEnumified => false;
+
+			public override string TypeName => type.Name;
+
+			public override string Name { get; set; }
+
+			public override string Value => null;
+
+			public override string Visibility => "public";
+
+			Parameter setterParameter = new Parameter ("value", "java.lang.String", "string", false);
+
+			protected override Parameter SetterParameter => setterParameter;
+		}
+
 		XamarinAndroidCodeGenerator generator;
 		StringBuilder builder;
 		StringWriter writer;
@@ -74,9 +111,7 @@ namespace generatortests
 			generator = new XamarinAndroidCodeGenerator ();
 			builder = new StringBuilder ();
 			writer = new StringWriter (builder);
-			options = new CodeGenerationOptions {
-
-			};
+			options = new CodeGenerationOptions ();
 		}
 
 		[Test]
@@ -107,6 +142,55 @@ namespace generatortests
 	get { return typeof (Com.MyPackage.Foo); }
 }
 
+", builder.ToString ());
+		}
+
+		[Test]
+		public void WriteFieldIdField ()
+		{
+			var @class = new TestClass ("java.lang.Object", "com.mypackage.foo");
+			var field = new TestField (@class, "bar");
+
+			generator.WriteFieldIdField (field, writer, string.Empty, options);
+
+			Assert.AreEqual (@"static IntPtr bar_jfieldId;
+", builder.ToString ());
+		}
+
+		[Test]
+		public void WriteFieldGetBody ()
+		{
+			var @class = new TestClass ("java.lang.Object", "com.mypackage.foo");
+			var field = new TestField (@class, "bar");
+			field.Validate (options, new GenericParameterDefinitionList ());
+			options.ContextTypes.Push (@class);
+			generator.WriteFieldGetBody (field, writer, string.Empty, options);
+			options.ContextTypes.Pop ();
+
+			Assert.AreEqual (@"if (bar_jfieldId == IntPtr.Zero)
+	bar_jfieldId = JNIEnv.GetFieldID (class_ref, ""bar"", ""foo"");
+return JNIEnv.GetFooField (((global::Java.Lang.Object) this).Handle, bar_jfieldId);
+", builder.ToString ());
+		}
+
+		[Test]
+		public void WriteFieldSetBody ()
+		{
+			var @class = new TestClass ("java.lang.Object", "com.mypackage.foo");
+			var field = new TestField (@class, "bar");
+			field.Validate (options, new GenericParameterDefinitionList ());
+			options.ContextTypes.Push (@class);
+			generator.WriteFieldSetBody (field, writer, string.Empty, options);
+			options.ContextTypes.Pop ();
+
+			Assert.AreEqual (@"if (bar_jfieldId == IntPtr.Zero)
+	bar_jfieldId = JNIEnv.GetFieldID (class_ref, ""bar"", ""foo"");
+IntPtr native_value = JNIEnv.NewString (value);
+try {
+	JNIEnv.SetField (((global::Java.Lang.Object) this).Handle, bar_jfieldId, native_value);
+} finally {
+	JNIEnv.DeleteLocalRef (native_value);
+}
 ", builder.ToString ());
 		}
 	}
