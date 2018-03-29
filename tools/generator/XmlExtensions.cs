e1548b024c2c7d9d9e9505c09f3d53830e652a80
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-03-29 10:38:34 -0500

    [generator] initial Method unit tests
    DONE:
    - StreamWriter sw -> TextWriter writer
    - Return types: void, int, string
    - static, final, abstract
    - Move Generate* methods out of Method into CodeGenerator
    - Asyncify, Protected, Deprecated, ManagedReturn, Enumified
    - Got rid of *MethodSupport in favor of extension methods
    - Wrote Base test class
    
    TODO:
    - MethodBase have Generate* methods?
    - Parameters
    - Code coverage of all TestMethod members

---
+++ tools/generator/XmlExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Linq;
+using Xamarin.Android.Tools;
+
+namespace MonoDroid.Generation
+{
+	internal static class XmlExtensions
+	{
+		public static string Deprecated (this XElement elem) => 
+			elem.XGetAttribute ("deprecated") != "not deprecated" ? elem.XGetAttribute ("deprecated") : null;
+
+		public static string Visibility (this XElement elem) => elem.XGetAttribute ("visibility");
+
+		public static GenericParameterDefinitionList GenericArguments (this XElement elem)
+		{
+			var tps = elem.Element ("typeParameters");
+			return tps != null ? GenericParameterDefinitionList.FromXml (tps) : null;
+		}
+	}
+}
