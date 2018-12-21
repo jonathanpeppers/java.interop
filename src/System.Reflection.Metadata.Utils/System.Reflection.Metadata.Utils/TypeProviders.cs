7adb43bb973ee93816c8c02e5055f9e20eff136d
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-12-21 16:56:21 -0600

    Update TypeProviders.cs

--- src/System.Reflection.Metadata.Utils/System.Reflection.Metadata.Utils/TypeProviders.cs
+++ src/System.Reflection.Metadata.Utils/System.Reflection.Metadata.Utils/TypeProviders.cs
@@ -6,7 +6,46 @@ namespace System.Reflection.Metadata.Utils
 	{
 		public Type GetPrimitiveType (PrimitiveTypeCode typeCode)
 		{
-			throw new NotImplementedException ();
+			switch (typeCode) {
+				case PrimitiveTypeCode.Boolean:
+					return typeof (bool);
+				case PrimitiveTypeCode.Byte:
+					return typeof (byte);
+				case PrimitiveTypeCode.Char:
+					return typeof (char);
+				case PrimitiveTypeCode.Double:
+					return typeof (double);
+				case PrimitiveTypeCode.Int16:
+					return typeof (short);
+				case PrimitiveTypeCode.Int32:
+					return typeof (int);
+				case PrimitiveTypeCode.Int64:
+					return typeof (long);
+				case PrimitiveTypeCode.IntPtr:
+					return typeof (IntPtr);
+				case PrimitiveTypeCode.Object:
+					return typeof (object);
+				case PrimitiveTypeCode.SByte:
+					return typeof (sbyte);
+				case PrimitiveTypeCode.Single:
+					return typeof (float);
+				case PrimitiveTypeCode.String:
+					return typeof (string);
+				case PrimitiveTypeCode.TypedReference:
+					return typeof (TypedReference);
+				case PrimitiveTypeCode.UInt16:
+					return typeof (ushort);
+				case PrimitiveTypeCode.UInt32:
+					return typeof (uint);
+				case PrimitiveTypeCode.UInt64:
+					return typeof (ulong);
+				case PrimitiveTypeCode.UIntPtr:
+					return typeof (UIntPtr);
+				case PrimitiveTypeCode.Void:
+					return typeof (void);
+				default:
+					throw new ArgumentOutOfRangeException (nameof (typeCode));
+			}
 		}
 
 		public Type GetTypeFromDefinition (MetadataReader reader, TypeDefinitionHandle handle, byte rawTypeKind)
@@ -19,6 +58,14 @@ namespace System.Reflection.Metadata.Utils
 		{
 			var type = reader.GetTypeReference (handle);
 			return Type.GetType (type.FullName (reader), throwOnError: true);
+		}
+	}
+
+	public class SimpleArrayProvider : SimpleTypeProvider, ISZArrayTypeProvider<Type>
+	{
+		public Type GetSZArrayType (Type elementType)
+		{
+			return elementType.MakeArrayType ();
 		}
 	}
 }
